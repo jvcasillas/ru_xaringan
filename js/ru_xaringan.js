c6c6e3f1354542ae8ceba52e9d915e6eebff0cae
jvcasillas <casillas@Josephs-MBP-2.fios-router.home>
2017-11-01 11:20:10 -0400

    add favicon js stuff

--- js/ru_xaringan.js
+++ js/ru_xaringan.js
@@ -3,3 +3,6 @@ remark.macros.scale = function (percentage) {
   var url = this;
   return '<img src="' + url + '" style="width: ' + percentage + '" />';
 };
+
+
+<script src="https://use.fontawesome.com/5235085b15.js"></script>
